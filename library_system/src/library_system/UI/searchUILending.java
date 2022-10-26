a14d13a8de714f8d5ab4a9505669389342bc4a13
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2022-10-26 19:47:22 +0530

    finished search UI
    complete all function in search UI newly use date choose

--- library_system/src/library_system/UI/searchUILending.java
+++ library_system/src/library_system/UI/searchUILending.java
@@ -11,11 +11,14 @@ import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -27,6 +30,7 @@ import library_system.home_menu;
 import library_system.searchUI;
 import library_system.sqlConnection;
 import net.proteanit.sql.DbUtils;
+import com.toedter.calendar.JDateChooser;
 
 public class searchUILending extends JFrame {
 
@@ -58,6 +62,7 @@ public class searchUILending extends JFrame {
 			private JTable table;
 			private String searchByString = "book_ID";
 			private String serchString = "1";
+			private JDateChooser dateChooser;
 			
 			public void setColouronMouse( JPanel panel) { //create a method to change button color when mouse is on it
 				panel.setBackground(new java.awt.Color(115, 163, 239));		
@@ -117,6 +122,10 @@ public class searchUILending extends JFrame {
 	 * Create the frame.
 	 */
 	public searchUILending() {
+		
+		
+		java.sql.Date today = new java.sql.Date(System.currentTimeMillis() ); // get the local date       
+	    System.out.println(today);   
 		
 		connection = sqlConnection.dbConnector(); //connect with sql server
 
@@ -312,7 +321,7 @@ public class searchUILending extends JFrame {
 						
 						try {
 							
-							if (searchByString == "M.Mem_ID"  ) {
+							if (searchByString == "M.Mem_ID"  ) { //member id is integer
 								
 								serchString = textFieldSearch.getText();
 								String queryString = "SELECT \r\n"
@@ -335,6 +344,7 @@ public class searchUILending extends JFrame {
 										+ "WHERE "+searchByString+" = '"+serchString+"'\r\n"
 										+ "ORDER BY L.Mem_ID;";
 								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
 								
 								PreparedStatement pStatement = connection.prepareStatement(queryString);
 								ResultSet rsResultset = pStatement.executeQuery(); 
@@ -344,7 +354,7 @@ public class searchUILending extends JFrame {
 								pStatement.close();
 								rsResultset.close();
 								
-							}else if (searchByString == "B.book_ID") {
+							}else if (searchByString == "B.book_ID" || searchByString == "M.F_name" ) {
 								
 								serchString = textFieldSearch.getText();
 								String queryString = "SELECT \r\n"
@@ -364,9 +374,10 @@ public class searchUILending extends JFrame {
 										+ "	ON L.book_ID = B.book_ID\r\n"
 										+ "LEFT JOIN members M\r\n"
 										+ "	ON L.Mem_ID = M.Mem_ID \r\n"
-										+ "WHERE "+searchByString+" = '%"+serchString+"%'\r\n"
+										+ "WHERE "+searchByString+" = '"+serchString+"'\r\n"
 										+ "ORDER BY L.Mem_ID;";
 								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
 								
 								PreparedStatement pStatement = connection.prepareStatement(queryString);
 								ResultSet rsResultset = pStatement.executeQuery(); 
@@ -376,8 +387,163 @@ public class searchUILending extends JFrame {
 								pStatement.close();
 								rsResultset.close();
 								
-							}else {
+							}else if (searchByString == "lend_date"  ) {
 								
+								serchString = textFieldSearch.getText();
+								String queryString = "SELECT \r\n"
+										+ "B.book_ID AS 'Book ID',\r\n"
+										+ "M.Mem_ID AS 'Member ID',\r\n"
+										+ "F_name AS 'First Name',\r\n"
+										+ "L_name AS 'Last Name',\r\n"
+										+ "Contact_no AS 'Contact Number',\r\n"
+										+ "lend_date AS 'Lend Date',\r\n"
+										+ "resubmit_date AS 'Re Submit Date',\r\n"
+										+ "B_name AS 'Book Name',\r\n"
+										+ "publish_year AS 'Published Year',\r\n"
+										+ "state AS 'Book Status'\r\n"
+										+ "\r\n"
+										+ "FROM lending L\r\n"
+										+ "LEFT JOIN books B\r\n"
+										+ "	ON L.book_ID = B.book_ID\r\n"
+										+ "LEFT JOIN members M\r\n"
+										+ "	ON L.Mem_ID = M.Mem_ID \r\n"
+										+ "WHERE lend_date BETWEEN '2000-01-01' AND '"+today+"'\r\n"
+										+ "ORDER BY lend_date;";
+								
+								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
+								
+								PreparedStatement pStatement = connection.prepareStatement(queryString);
+								ResultSet rsResultset = pStatement.executeQuery(); 
+								table.setModel(DbUtils.resultSetToTableModel(rsResultset));
+								
+								
+								pStatement.close();
+								rsResultset.close();
+								
+								
+							}else if (searchByString == "resubmit_date" ) {
+								
+								serchString = textFieldSearch.getText();
+								String queryString = "SELECT \r\n"
+										+ "B.book_ID AS 'Book ID',\r\n"
+										+ "M.Mem_ID AS 'Member ID',\r\n"
+										+ "F_name AS 'First Name',\r\n"
+										+ "L_name AS 'Last Name',\r\n"
+										+ "Contact_no AS 'Contact Number',\r\n"
+										+ "lend_date AS 'Lend Date',\r\n"
+										+ "resubmit_date AS 'Re Submit Date',\r\n"
+										+ "B_name AS 'Book Name',\r\n"
+										+ "publish_year AS 'Published Year',\r\n"
+										+ "state AS 'Book Status'\r\n"
+										+ "\r\n"
+										+ "FROM lending L\r\n"
+										+ "LEFT JOIN books B\r\n"
+										+ "	ON L.book_ID = B.book_ID\r\n"
+										+ "LEFT JOIN members M\r\n"
+										+ "	ON L.Mem_ID = M.Mem_ID \r\n"
+										+ "WHERE \r\n"
+										+ "(resubmit_date BETWEEN '2000-01-01' AND '"+today+"') \r\n"
+										+ "AND \r\n"
+										+ "( state = 'NOT_AVALIABLE')\r\n"
+										+ "ORDER BY lend_date;\r\n"
+										+ "";
+								
+								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
+								
+								PreparedStatement pStatement = connection.prepareStatement(queryString);
+								ResultSet rsResultset = pStatement.executeQuery(); 
+								table.setModel(DbUtils.resultSetToTableModel(rsResultset));
+								
+								
+								pStatement.close();
+								rsResultset.close();
+								
+								
+							}
+							
+							else if (searchByString == "state" ) {
+								
+								//serchString = textFieldSearch.getText();
+								String queryString = "SELECT \r\n"
+										+ "B.book_ID AS 'Book ID',\r\n"
+										+ "M.Mem_ID AS 'Member ID',\r\n"
+										+ "F_name AS 'First Name',\r\n"
+										+ "L_name AS 'Last Name',\r\n"
+										+ "Contact_no AS 'Contact Number',\r\n"
+										+ "lend_date AS 'Lend Date',\r\n"
+										+ "resubmit_date AS 'Re Submit Date',\r\n"
+										+ "B_name AS 'Book Name',\r\n"
+										+ "publish_year AS 'Published Year',\r\n"
+										+ "state AS 'Book Status'\r\n"
+										+ "\r\n"
+										+ "FROM lending L\r\n"
+										+ "LEFT JOIN books B\r\n"
+										+ "	ON L.book_ID = B.book_ID\r\n"
+										+ "LEFT JOIN members M\r\n"
+										+ "	ON L.Mem_ID = M.Mem_ID \r\n"
+										+ "WHERE \r\n"
+										+ "state = 'NOT_AVALIABLE'\r\n"
+										+ "ORDER BY lend_date;\r\n";
+								
+								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
+								
+								PreparedStatement pStatement = connection.prepareStatement(queryString);
+								ResultSet rsResultset = pStatement.executeQuery(); 
+								table.setModel(DbUtils.resultSetToTableModel(rsResultset));
+								
+								
+								pStatement.close();
+								rsResultset.close();
+								
+								
+							}else if (searchByString == "byDate") {
+								
+								//serchString = textFieldSearch.getText();
+								
+								String selectDateString = ((JTextField)dateChooser.getDateEditor().getUiComponent()).getText(); ;
+								
+								
+								String queryString = "SELECT \r\n"
+										+ "B.book_ID AS 'Book ID',\r\n"
+										+ "M.Mem_ID AS 'Member ID',\r\n"
+										+ "F_name AS 'First Name',\r\n"
+										+ "L_name AS 'Last Name',\r\n"
+										+ "Contact_no AS 'Contact Number',\r\n"
+										+ "lend_date AS 'Lend Date',\r\n"
+										+ "resubmit_date AS 'Re Submit Date',\r\n"
+										+ "B_name AS 'Book Name',\r\n"
+										+ "publish_year AS 'Published Year',\r\n"
+										+ "state AS 'Book Status'\r\n"
+										+ "\r\n"
+										+ "FROM lending L\r\n"
+										+ "LEFT JOIN books B\r\n"
+										+ "	ON L.book_ID = B.book_ID\r\n"
+										+ "LEFT JOIN members M\r\n"
+										+ "	ON L.Mem_ID = M.Mem_ID \r\n"
+										+ "WHERE \r\n"
+										+ "(resubmit_date BETWEEN '2000-01-01' AND '"+selectDateString+"') \r\n"
+										+ "AND \r\n"
+										+ "( state = 'NOT_AVALIABLE')\r\n"
+										+ "ORDER BY lend_date;";
+								
+								System.out.println(queryString); //use this to check the errors in query
+								System.out.println("");
+								
+								PreparedStatement pStatement = connection.prepareStatement(queryString);
+								ResultSet rsResultset = pStatement.executeQuery(); 
+								table.setModel(DbUtils.resultSetToTableModel(rsResultset));
+								
+								
+								pStatement.close();
+								rsResultset.close();
+								
+							}
+							
+							else { 
+								showTable();
 							}
 							
 								
@@ -420,15 +586,23 @@ public class searchUILending extends JFrame {
 							searchByString = "B.book_ID";
 						}else if (searchByString == "Member ID") {
 							searchByString = "M.Mem_ID";
+						}else if (searchByString == "Member First Name") {
+							searchByString = "M.F_name";
 						}else if (searchByString =="Lend Date") {
 							searchByString = "lend_date";
-						}else if (searchByString == "Resubmit Date") {
+							JOptionPane.showMessageDialog(null,"This Will show you book lend history until today. click Search button");
+						}else if (searchByString == "Not Resubmit until Today") {
 							searchByString = "resubmit_date";
-						}else if (searchByString == "Author First Name") {
-							searchByString = "F_name";
+							JOptionPane.showMessageDialog(null,"This Will show you Not Submited book history until today. click Search button");
+						}else if (searchByString == "All Not Submited Books") {
+							searchByString = "state";
+							JOptionPane.showMessageDialog(null,"This Will show you ALL Not Submited book history. click Search button");
+						}else if (searchByString == "Not Submitted as at Date") {
+							searchByString = "byDate";
+							JOptionPane.showMessageDialog(null,"This Will show you ALL Not Submited book history as at Selected Date. select date and click Search button");
 						}
 						else {
-							searchByString = "book_ID";
+							searchByString = "B.book_ID";
 						}	
 						
 					}
@@ -440,9 +614,11 @@ public class searchUILending extends JFrame {
 				
 				comboBoxSearchTable.addItem("Book ID");
 				comboBoxSearchTable.addItem("Member ID");
+				comboBoxSearchTable.addItem("Member First Name");
 				comboBoxSearchTable.addItem("Lend Date");
-				comboBoxSearchTable.addItem("Resubmit Date");
-				comboBoxSearchTable.addItem("Author First Name");
+				comboBoxSearchTable.addItem("Not Resubmit until Today");
+				comboBoxSearchTable.addItem("All Not Submited Books");
+				comboBoxSearchTable.addItem("Not Submitted as at Date");
 				comboBoxSearchTable.setSelectedItem("Book ID");
 				
 				JScrollPane scrollPane = new JScrollPane();
@@ -451,6 +627,19 @@ public class searchUILending extends JFrame {
 				
 				table = new JTable();
 				scrollPane.setViewportView(table);
+				
+				
+				JLabel lblLabelFrom = new JLabel("Select Date to find by \"Not Submitted as at Date\"");
+				lblLabelFrom.setFont(new Font("Trebuchet MS", Font.BOLD, 15));
+				lblLabelFrom.setBounds(423, 95, 366, 14);
+				panelbody.add(lblLabelFrom);
+				
+				dateChooser = new JDateChooser();
+				dateChooser.setDateFormatString("yyyy-MM-dd");
+				dateChooser.setBounds(476, 119, 218, 27);
+				panelbody.add(dateChooser);
+				
+				//dateToLend ;
 				
 				showTable();
 				//Back to Home BUtton ENDS ---------------------------------------------------------------------
